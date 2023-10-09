0acc3117f748505e445d90f17fb54295aa625fa4
amorags <115067025+amorags@users.noreply.github.com>
2023-10-09 16:24:48 +0200

    NavBar Update 1

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -9,6 +9,17 @@ export class NavBarComponent  implements OnInit {
 
   constructor() { }
 
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  menuItemClicked(item: string) {
+    // Handle the menu item click event here
+    console.log(`Clicked on ${item}`);
+    // You can implement specific actions for each menu item
+  }
   ngOnInit() {}
 
 }
