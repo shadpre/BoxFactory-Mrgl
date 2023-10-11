1ebd890676c9b8b645ed7b7e1f253fda664cc4ed
amorags <115067025+amorags@users.noreply.github.com>
2023-10-11 21:42:13 +0200

    This is annoying

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -8,28 +8,82 @@ namespace PlaywrightTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
+
 public class Tests : PageTest
 {
-    [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
-    {
-        await Page.GotoAsync("https://playwright.dev");
+   
+	[Test]
+    public async Task BoxCreationValid()
+     {
+        await Page.GotoAsync("http://localhost:4200/");
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        await Page.Locator("ion-fab-button").GetByRole(AriaRole.Img).Nth(1).ClickAsync();
 
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "insert a name for the box please" }).ClickAsync();
 
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+        await Page.Locator("#ion-input-14").FillAsync("Naming");
 
-        // Click the get started link.
-        await getStarted.ClickAsync();
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "please add a description" }).ClickAsync();
 
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        await Page.Locator("#ion-input-15").FillAsync("Working Now");
+
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "please insert the length of the box" }).ClickAsync();
+
+        await Page.Locator("#ion-input-16").FillAsync("450");
+
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "please insert the width of the box" }).ClickAsync();
+
+        await Page.Locator("#ion-input-17").FillAsync("459");
+
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "please insert the height of the box" }).ClickAsync();
+
+        await Page.Locator("#ion-input-18").FillAsync("900");
+
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "please add a price" }).ClickAsync();
+
+        await Page.Locator("#ion-input-19").FillAsync("500");
+
+        await Page.Locator("#ion-overlay-1").GetByRole(AriaRole.Button, new() { Name = "send" }).ClickAsync();
+
     }
+	
+	[Test]
+		 public async Task BoxCreationInvalid()
+  		  {
+        await Page.GotoAsync("http://localhost:4200/");
+
+        await Page.Locator("ion-fab-button").GetByRole(AriaRole.Img).Nth(1).ClickAsync();
+
+        await Page.Locator("#ion-overlay-1 label").Filter(new() { HasText = "insert a name for the box please" }).ClickAsync();
+
+        await Page.Locator("#ion-input-13").FillAsync("bad");
+
+        await Page.Locator("#ion-input-14").ClickAsync();
+
+        await Page.Locator("#ion-input-14").FillAsync("tester");
+
+        await Page.Locator("#ion-input-15").ClickAsync();
+
+        await Page.Locator("#ion-input-15").FillAsync("455");
+
+        await Page.Locator("#ion-input-16").ClickAsync();
+
+        await Page.Locator("#ion-input-16").FillAsync("4455");
+
+        await Page.Locator("#ion-input-17").ClickAsync();
+
+        await Page.Locator("#ion-input-17").FillAsync("555");
+
+        await Page.Locator("#ion-input-18").ClickAsync();
+
+        await Page.Locator("#ion-input-18").FillAsync("4555");
+
+        await Page.Locator("#ion-overlay-1").GetByRole(AriaRole.Button, new() { Name = "send" }).ClickAsync();
+
+        Assert.Fail("Box creation should have failed with an invalid input.");
+
+    }
+		
 }
 
 /*
