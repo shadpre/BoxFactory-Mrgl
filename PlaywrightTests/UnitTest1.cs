313aef56a42876e1a52f72030b8b349bc02ec9f2
amorags <115067025+amorags@users.noreply.github.com>
2023-10-12 14:01:38 +0200

    Update, Delete, Create Working tests

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -12,38 +12,61 @@ namespace PlaywrightTests;
 public class Tests : PageTest
 {
   
-	[Test]
-    public async Task BoxCreationValid()
+	[Test, Order(1)]
+    public async Task pwCreateBox()
      {
         await Page.GotoAsync("http://localhost:4200/");
 
         await Page.Locator("ion-fab-button").GetByRole(AriaRole.Img).Nth(1).ClickAsync();
 
-        await Page.GetByTestId("boxName").Locator("input").Nth(1).FillAsync("Textbox");
+        await Page.GetByTestId("boxName").Locator("input").Nth(1).FillAsync("Test");
 
-        await Page.GetByTestId("boxingtester").Locator("input").Nth(1).FillAsync("En Box");
+        await Page.GetByTestId("boxingtester").Locator("input").Nth(1).FillAsync("Test");
 
-		await Page.GetByTestId("boxLength").Locator("input").Nth(1).FillAsync("350");
+		await Page.GetByTestId("boxLength").Locator("input").Nth(1).FillAsync("220");
 
-		await Page.GetByTestId("boxHeight").Locator("input").Nth(1).FillAsync("350");
+		await Page.GetByTestId("boxHeight").Locator("input").Nth(1).FillAsync("222");
 
-		await Page.GetByTestId("boxWidth").Locator("input").Nth(1).FillAsync("350");
+		await Page.GetByTestId("boxWidth").Locator("input").Nth(1).FillAsync("220");
 
-		await Page.GetByTestId("boxPrice").Locator("input").Nth(1).FillAsync("350");
+		await Page.GetByTestId("boxPrice").Locator("input").Nth(1).FillAsync("220");
 
 		await Page.Locator("#ion-overlay-1").GetByRole(AriaRole.Button, new() { Name = "send" }).ClickAsync();
-
+		
+		await Page.ReloadAsync();
  	 }
-
-
-[Test]
-   public async Task pwDelete()
+	
+	[Test, Order(2)]
+    public async Task pwUpdateBox()
     {
         await Page.GotoAsync("http://localhost:4200/");
 
-        await Page.GetByTestId("boxDeleter").Nth(1).ClickAsync();
+        await Page.Locator("ion-card").Filter(new() { HasText = "TestLength: 220 mm - Width: 220 mm - Height: 222 mmPrice: 220 kr.In storage: Add" }).Locator("svg").Nth(1).ClickAsync();
+ 		
+		await Page.GetByTestId("updateName").Locator("input").Nth(1).FillAsync("Test");
 
-	}
+		await Page.GetByTestId("updateHeight").Locator("input").Nth(1).FillAsync("2222");
+
+        await Page.Locator("#ion-overlay-1").GetByRole(AriaRole.Button, new() { Name = "Update" }).ClickAsync();
+
+		await Page.ReloadAsync();
+		
+	
+
+    }
+
+
+
+ 	[Test, Order(3)]
+    public async Task pwDeleteBox()
+    {
+        await Page.GotoAsync("http://localhost:4200/");
+
+        await Page.Locator("ion-card").Filter(new() { HasText = "TestLength: 220 mm - Width: 220 mm - Height: 2222 mmPrice: 220 kr.In storage: Add" }).GetByTestId("boxDeleter").GetByRole(AriaRole.Img).ClickAsync();
+
+    }
+
+
 
 
 
@@ -63,32 +86,7 @@ public class Tests : PageTest
 
 }
 	
-/*	[Test]
-		 public async Task BoxCreationInvalid()
-  		  {
-           await Page.GotoAsync("http://localhost:4200/");
 
-        await Page.Locator("ion-fab-button").GetByRole(AriaRole.Img).Nth(1).ClickAsync();
-
-        await Page.GetByTestId("boxName").Locator("input").Nth(1).FillAsync("Textbox");
-
-        await Page.GetByTestId("boxingtester").Locator("input").Nth(1).FillAsync("En Box");
-
-		await Page.GetByTestId("boxLength").Locator("input").Nth(1).FillAsync("350");
-
-		await Page.GetByTestId("boxHeight").Locator("input").Nth(1).FillAsync("350");
-
-		await Page.GetByTestId("boxWidth").Locator("input").Nth(1).FillAsync("350");
-
-		await Page.GetByTestId("boxPrice").Locator("input").Nth(1).FillAsync("350");
-
-		await Page.Locator("#ion-overlay-1").GetByRole(AriaRole.Button, new() { Name = "send" }).ClickAsync();
-
-		Assert.False;
-
-    }
-		
-}
 
 /*
  dotnet tool install --global Microsoft.Playwright.CLI
